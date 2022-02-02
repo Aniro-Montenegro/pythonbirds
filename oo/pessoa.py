6f556ad64bf1cfe27ee6e8c94112d17658aaa4d3
79Aniro <capablanca1@bol.com.br>
2022-02-02 11:29:15 -0300

    Atributo de Classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,18 @@
 class Pessoa:
-    def __init__(self,*filhos, nome=None, idade=35):
+    olhos = 2
+
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
-        self.filhos=list(filhos)
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    marisa=Pessoa(nome='Marisa')
-    aniro = Pessoa(marisa,nome='Aniro')
+    marisa = Pessoa(nome='Marisa')
+    aniro = Pessoa(marisa, nome='Aniro')
     print(Pessoa.cumprimentar(aniro))
     print(id(aniro))
     print(aniro.cumprimentar())
@@ -18,11 +20,24 @@ if __name__ == '__main__':
     print(aniro.idade)
     for filho in aniro.filhos:
         print(filho.nome)
-    aniro.sobrenome='Montenegro'
+    aniro.sobrenome = 'Montenegro'
     print(aniro.__dict__)
     print(marisa.__dict__)
     del aniro.filhos
     print(aniro.__dict__)
     print(marisa.__dict__)
-
-
+    print(Pessoa.olhos)
+    print(aniro.olhos)
+    print(marisa.olhos)
+    print(id(Pessoa.olhos))
+    print(id(aniro.olhos))
+    print(id(marisa.olhos))
+    aniro.olhos=1
+    print(aniro.__dict__)
+    print(marisa.__dict__)
+    Pessoa.olhos=3
+    print(Pessoa.olhos)
+    print(aniro.olhos)
+    print(marisa.olhos)
+    print(aniro.__dict__)
+    print(marisa.__dict__)
