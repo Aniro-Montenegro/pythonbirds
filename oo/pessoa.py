4e2106469f95f6fb5b3b01df2aa4e1f51b10cbf7
79Aniro <capablanca1@bol.com.br>
2022-02-02 11:10:55 -0300

    Atributo Complexo

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self,*filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos=list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Aniro Montenegro')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = "Aniro"
-    print(p.nome)
-    print(p.idade)
+    marisa=Pessoa(nome='Marisa')
+    aniro = Pessoa(marisa,nome='Aniro Montenegro')
+    print(Pessoa.cumprimentar(aniro))
+    print(id(aniro))
+    print(aniro.cumprimentar())
+    print(aniro.nome)
+    print(aniro.idade)
+    for filho in aniro.filhos:
+        print(filho.nome)
